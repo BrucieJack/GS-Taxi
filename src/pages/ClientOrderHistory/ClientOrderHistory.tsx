13a6cf04bcc8fa51877372b5b9f14b32a514e8e6
BrucieJack <nikitos17082002@mail.ru>
2022-12-23 00:51:34 +0300

    l

--- src/pages/ClientOrderHistory/ClientOrderHistory.tsx
+++ src/pages/ClientOrderHistory/ClientOrderHistory.tsx
@@ -12,7 +12,7 @@ import "typeface-rasa";
 
 import BasicTable from "../../components/table/Table";
 
-const colums2 = ["Date", "From", "To", "Client", "Cost"];
+const colums2 = ["Date", "From", "To", "Driver", "Rating", "Cost", "Report"];
 
 //доделать
 
@@ -35,10 +35,16 @@ export const CliOrderHstory = () => {
               <Text>Lenina 53</Text>
             </Cell>
             <Cell align="center">
-              <Text>Ivan Ivanov</Text>
+              <Text>7.8</Text>
             </Cell>
             <Cell align="center">
               <Text>$ 4.2</Text>
+            </Cell>
+            <Cell align="center">
+              <Text>$ 4.2</Text>
+            </Cell>
+            <Cell align="center">
+              <Text>report</Text>
             </Cell>
           </Row>
         </BasicTable>
